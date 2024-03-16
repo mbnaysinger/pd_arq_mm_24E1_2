9daeee6c5ddb1eac251c35fb463e889a26d04952
Maike Naysinger Borges <76126951+mbnaysinger@users.noreply.github.com>
2024-03-15 23:11:41 -0300

    Delete payment directory

--- payment/src/main/java/com/store/payment/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.store.payment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.store.payment.domain.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long>{
-
-}
--- payment/src/main/java/com/store/payment/service/PaymentService.java
+++
@@ -1,6 +0,0 @@
-package com.store.payment.service;
-
-import com.store.payment.domain.Payment;
-
-public interface PaymentService extends GenericService<Payment> {
-}
